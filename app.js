471ef97fe5787753b33840cf306eaa40d871a764
Gabriel Acosta <gabriacos@gmail.coma>
2023-08-09 12:36:37 -0300

    Se inicia el trabajo Practico

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+const app = express();
+
+require('ejs');
+app.set('view engine',"ejs");
+
+app.use(express.static('public'));
+
+//midlewares
+app.use(express.urlencoded({extended:true}));
+app.use(express.json);
+app.use(cors());
+//variables de entorno
+dotenv.config({path:'./env/env'})
+
+const port = process.env.PORT || 3000;
+const host = 'localhost';
+
+app.listen(port,host,() =>{
+    console.log(`Servidor corriento en http://${host}:${port}/` );
+})
---
+++ db.js
@@ -0,0 +1,17 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const dotenv = require('dotenv').config('./env/.env')
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host:process.env.DB_HOST,
+        dialect:process.env.DB_DIALECT || 'mysql'
+    }
+);
+module.exports = {
+    sequelize,
+    Model,
+    DataTypes,
+}
