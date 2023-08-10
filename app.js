e6d030af52eb5713c4b2484ab37422a773f806b4
Gabriel Acosta <gabriacos@gmail.coma>
2023-08-10 09:50:00 -0300

    Se creó la conexión a la base de datos

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const { sequelize } = require('./db');
 
 const app = express();
 
@@ -14,11 +15,15 @@ app.use(express.static('public'));
 //midlewares
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 //variables de entorno
 dotenv.config({path:'./env/.env'})
 
 app.use(require('./routes/router'));
+
+sequelize.authenticate()
+    .then(()=> console.log('Conexión a la base de datos exitosa'))
+    .catch((error)=>console.log('Error al conectar a la base de datos'))
 
 const port = process.env.PORT;
 const host = 'localhost';
---
+++ models/images.js
@@ -0,0 +1,35 @@
+const {sequelize, DataTypes} = require('../db')
+const Images = sequelize.define('Images',{
+    id:{
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+    },
+    rutaImagen:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    createdAt:{
+        type: DataTypes.DATE,
+        allowNull:false,
+        defaultValue:sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updateAt:{
+        type:DataTypes.DATE,
+        allowNull:true,
+    }
+    ,
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }}
+    ,
+{
+    createdAt:true,
+    updatedAt:true,
+    deletedAt:true,
+    tableName:'reservas'
+});
+Images.sync();
+
+module.exports = Images;
